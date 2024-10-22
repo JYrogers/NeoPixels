f1877116afd6ace62eb427d459497e1e9e330fda
JYrogers <2025rogers.jacobi@fcboe.org>
2024-10-22 13:36:55 -0400

    Update flame.py

--- flame.py
+++ flame.py
@@ -3,25 +3,31 @@ import time
 from neopixel import NeoPixel
 import random
 
-np = NeoPixel(board.D2, 30, auto_write = False, brightness=3)
+np = NeoPixel(board.D2, 30, auto_write = False, brightness=1)
 
 
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-white = (255,255,255)
-orange = (238,96,0)
-yellow = (255,255,0)
 
-def flame():
-    doo = random.randint(1,50)
-    for i in range(doo):
-        np.fill(orange)
-        np.show()
-        too = random.randint(1,8)
-        doo = doo / 45
-        np.fill(yellow)
-        np.show()
-        time.sleep(doo)        
+red = ((255,0,0))
+blue = ((0,255,0))
+green = ((0,0,255))
+white = ((255,255,255))
+orange = (255,69,0)
+yellow = (255,255,10)
+def flame(bg = (yellow), fg = (255,69,0) , delay = 0.1, fire = 4):
+    for i in  range(fire):
+              won = random.randint(0,29)
+              too = random.randint(0,29)
+              tree = random.randint(0,29)
+              forr = random.randint(0,29)
+              np.fill(bg)
+              np[won] = fg
+              np[too] = fg
+              np[tree] = fg
+              np[forr] = (255,165,0)
+              np.show()
+              time.sleep(delay)
+
+
 while True:
     flame()
+                         
